2da035b78aacc32ea25badb0db3a8c15ab90fbbe
Aaron Klinker <aaronklinker1@gmail.com>
2019-11-23 21:32:39 -0600

    Updated eslint config because of example project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,13 @@
-module.exports =  {
-  parser:  '@typescript-eslint/parser',
-  extends:  [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
-  plugins: [
-    "only-warn",
-  ],
-  parserOptions:  {
-    ecmaVersion:  2018,
-    sourceType:  'module',
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  plugins: ['only-warn'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  rules:  {
-    "@typescript-eslint/no-explicit-any": 'off',
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
   },
 };
