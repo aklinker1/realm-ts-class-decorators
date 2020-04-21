194b1b55a05d08fe00bdd3c6f6d03b4f5f77899d
Anudeep Palanki <anudeep.palanki@monsanto.com>
2020-04-21 10:17:01 -0500

    refactor as per code review inputs

--- src/__tests__/integration.test.ts
+++ src/__tests__/integration.test.ts
@@ -3,8 +3,8 @@ import { model, property } from '../decorators';
 import RealmModel from '../RealmModel';
 import UpdateMode = Realm.UpdateMode;
 
-@model('People')
-class People extends RealmModel {
+@model('Person')
+class Person extends RealmModel {
   @property({ type: 'string', primaryKey: true }) id: string | undefined;
   @property('string') firstName: string | undefined;
   @property('string') lastName: string | undefined;
@@ -13,16 +13,22 @@ class People extends RealmModel {
   }
 }
 
+let realm: Realm;
 describe('loads realm and add people object', () => {
+  afterAll(() => {
+    realm.close();
+  });
+
   it('es6 classes now work in NodeJS', async () => {
-    const realm = await Realm.open({
-      schema: [People],
+    realm = await Realm.open({
+      inMemory: true,
+      schema: [Person],
     });
     realm.write(() => {
-      realm.create(People.schema.name, { id: '1', firstName: 'John', lastName: 'Doe' }, UpdateMode.Modified);
+      realm.create(Person.schema.name, { id: '1', firstName: 'John', lastName: 'Doe' }, UpdateMode.Modified);
     });
 
-    const peoples = realm.objects<People>(People.schema.name);
+    const peoples = realm.objects<Person>(Person.schema.name);
 
     expect(peoples[0].fullName).toEqual('John Doe');
   });
